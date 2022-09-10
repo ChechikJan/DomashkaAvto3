c851fee35f257259d04bb7672b53e07abf99e3a0
Danila Kalay <ChechikLud@gmail.com>
2022-09-10 11:08:34 +0300

    OrderTest commit

---
+++ src/test/java/ru/netology/OrderTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderTest {
+    @Test
+    public void fillingOutTheForm() {
+        Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999/");
+        $("[name='name'").setValue("Смирнов Василий");
+        $("[name='phone'").setValue("+79003455555");
+        $("[role='presentation']").click();
+        $(byText("Продолжить")).click();
+        $("[data-test-id='order-success']").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+
+    }
+}
