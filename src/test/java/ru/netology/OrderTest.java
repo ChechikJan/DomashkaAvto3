4b9bd08d0900cbc78f5830f4d59de269279b4822
Danila Kalay <ChechikLud@gmail.com>
2022-09-11 13:09:56 +0300

    fix app commit

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -13,8 +13,8 @@ public class OrderTest {
     public void fillingOutTheForm() {
         Configuration.holdBrowserOpen=true;
         open("http://localhost:9999/");
-        $("[name='name'").setValue("Смирнов Василий");
-        $("[name='phone'").setValue("+79003455555");
+        $("[name='name']").setValue("Смирнов Василий");
+        $("[name='phone']").setValue("+79003455555");
         $("[role='presentation']").click();
         $(byText("Продолжить")).click();
         $("[data-test-id='order-success']").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
@@ -22,4 +22,8 @@ public class OrderTest {
 
 
     }
-}
+
+
+
+    }
+
