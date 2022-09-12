d70f70812bfaf238762e6822745fd3ddad2156b9
Danila Kalay <ChechikLud@gmail.com>
2022-09-12 10:47:06 +0300

    fix remarks commit

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -11,19 +11,17 @@ import static com.codeborne.selenide.Selenide.open;
 public class OrderTest {
     @Test
     public void fillingOutTheForm() {
-        Configuration.holdBrowserOpen=true;
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
-        $("[name='name']").setValue("Смирнов Василий");
+        $("[name='name']").setValue("Петров-Водкин Василий");
         $("[name='phone']").setValue("+79003455555");
-        $("[role='presentation']").click();
+        $(byText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй")).click();
         $(byText("Продолжить")).click();
         $("[data-test-id='order-success']").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
 
-
     }
 
 
-
-    }
+}
 
